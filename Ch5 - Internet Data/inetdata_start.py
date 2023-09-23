c2da8e93f782b2e886b526ad74e3044a1d298a95
Cherch <b.cherchiglia@gmail.com>
2023-09-23 19:47:58 -0400

    json data chapter five

--- Ch5 - Internet Data/inetdata_start.py
+++ Ch5 - Internet Data/inetdata_start.py
@@ -3,8 +3,14 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+# Import URL Lib
+import urllib.request
+
 def main():
-    pass # this is a placeholder, do-nothing statement
+    weburl = urllib.request.urlopen("http://bcportfolio.surge.sh")
+    print("result code:", weburl.getcode())
+    data = weburl.read
+    print(data)
 
 if __name__ == "__main__":
     main()
