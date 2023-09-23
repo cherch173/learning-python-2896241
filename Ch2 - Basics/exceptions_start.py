d34b25eb193dce895044ce6b4ea1471c65cae773
Cherch <b.cherchiglia@gmail.com>
2023-09-23 16:30:44 -0400

    code challenge completed

--- Ch2 - Basics/exceptions_start.py
+++ Ch2 - Basics/exceptions_start.py
@@ -5,10 +5,25 @@
 
 # Errors can happen in programs, and we need a clean way to handle them
 # TODO: This code will cause an error because you can't divide by zero:
+# x = 10 / 0
 
 # TODO: Exceptions provide a way of catching errors and then handling them in 
 # a separate section of the code to group them together
-
+# try:
+#     x = 10 /0
+# except: 
+#     print("Well that didn't work did it?")
 
 # TODO: You can also catch specific exceptions
+try:
+    answer = input("what should I divide 10 by?")
+    num = int(answer)
+    print(10/num)
+except ZeroDivisionError as e:
+    print("You can't divide by Zero, bb.")
+except ValueError as e:
+    print("You didn't give us a valid number, bud.")
+    print(e)
+finally:
+    print("this code always runs")
 
